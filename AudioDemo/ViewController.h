0a9c3a7f4631cc7fa77a6f53f12940b4a410f810
tylow <tylow@berkeley.edu>
2016-06-07 21:38:36 -0700

    first commit

---
+++ AudioDemo/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  AudioDemo
+//
+//  Created by Simon on 24/2/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController 
+
+@property (weak, nonatomic) IBOutlet UIButton *recordPauseButton;
+@property (weak, nonatomic) IBOutlet UIButton *stopButton;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+
+- (IBAction)recordPauseTapped:(id)sender;
+- (IBAction)stopTapped:(id)sender;
+- (IBAction)playTapped:(id)sender;
+
+@end
---
+++ AudioDemo/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  AudioDemo
+//
+//  Created by Simon on 24/2/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () {
+}
+
+@end
+
+@implementation ViewController
+@synthesize stopButton, playButton, recordPauseButton;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)recordPauseTapped:(id)sender {
+}
+
+- (IBAction)stopTapped:(id)sender {
+}
+
+- (IBAction)playTapped:(id)sender {
+}
+
+
+
+@end
